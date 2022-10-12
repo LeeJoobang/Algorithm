6d10f612b07fd9243c0ca3b22852c699b4ee8ac9
Joobang <hii5074@gmail.com>
2022-10-12 22:16:24 +0900

    update

--- 001_Swift/Algorithm/Algorithm/main.swift
+++ 001_Swift/Algorithm/Algorithm/main.swift
@@ -1,3 +1,2 @@
 import Foundation
 
-print("hello world")
---
+++ 001_Swift/Algorithm/Algorithm/sum.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/*
+ 두 수의 합
+ 
+ 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+ */
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    var sum = Int()
+    sum = num1 + num2
+    return sum
+}
